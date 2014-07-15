ed670a9cd015b10ac4189a4cd871728545017908
Isaac F <isaac.fernandez2@gmail.com>
2014-07-15 18:25:50 -0400

    Changed OPENSHIFT_INTERNAL i[/port to OPENSHIFT_NODEJS

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var io = require('socket.io'),
   connect = require('connect'),
   chatter = require('chatter');
 
- var port = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
- var ipadd = process.env.OPENSHIFT_INTERNAL_IP || "127.0.0.1";
+ var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+ var ipadd = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
 var app = connect().use(connect.static(__dirname + '/public')).listen(ipadd, port);
 var chat_room = io.listen(app);
@@ -11,7 +11,6 @@ var chat_room = io.listen(app);
 chatter.set_sockets(chat_room.sockets);
 
 chat_room.sockets.on('connection', function (socket) {
-  
   chatter.connect_chatter({
     socket: socket,
     username: socket.id,
